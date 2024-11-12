3fd0de9b01e32450020288d0c32cfa7127e044c4
nooshin <nooshinbry@gmail.com>
2024-11-12 17:30:23 +0330

    define router from react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
-import TaskItem from "./components/TaskItem.jsx";
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./pages/home/home.jsx";
+import EditTask from "./pages/edit-task/[taskId].jsx";
 
 function App() {
 
   return (
-   <div>
-       <TaskItem />
-   </div>
+   <Router>
+       <Routes>
+           <Route path='/' element={<Home />}></Route>
+           <Route path='/task/:id' element={<EditTask />}></Route>
+       </Routes>
+   </Router>
   )
+
 }
 
 export default App
