348fe42daaa3735fa23746d7a54291405a56cd5e
nooshin <nooshinbry@gmail.com>
2024-11-12 17:30:36 +0330

    built home page

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import TaskForm from "../../components/TaskForm.jsx";
+import TaskList from "../../components/TaskList.jsx";
+
+function Home() {
+
+    return (
+        <div>
+            <TaskForm />
+            <TaskList />
+        </div>
+    )
+
+}
+
+export default Home
